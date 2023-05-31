621e2da1d075acafb86e616312e1d65bbc0975d1
Kevin McQuate <kgmcquate@gmail.com>
2023-05-30 21:34:15 -0500

    adding loading box and css changes

--- lake-freeze-frontend/src/components/LoadingBox.js
+++ lake-freeze-frontend/src/components/LoadingBox.js
@@ -4,7 +4,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 import './LoadingBox.css'
 
-export function LoadingBox({}) {
+export function LoadingBox() {
     return (
         <div className='loading-box'>
             Loading Weather Data...

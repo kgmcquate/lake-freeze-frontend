8dc5cc45f6cc43e4f00c7aee619c8c476609d654
Kevin McQuate <kgmcquate@gmail.com>
2023-06-13 22:22:04 -0500

    changed lat and long to strings

--- lake-freeze-frontend/src/App.test.tsx
+++ lake-freeze-frontend/src/App.test.tsx
@@ -9,8 +9,8 @@ import { LakeInfoBox } from './components/LakeInfoBox';
 
 
 const mockLakes: Lake[] = [
-  {"longitude":-95.82589709999999,"id":3,"state_or_province":"minnesota","nearby_city_latitude":undefined,"max_depth_m":2.1336,"surface_area_m2":1088604.3776256,"latitude":46.7584275,"nearby_city_name":"detroit lakes","lake_name":"abbey","country":"USA","nearby_city_longitude":undefined},
-  {"longitude":-94.40303660000001,"id":7,"state_or_province":"minnesota","nearby_city_latitude":undefined,"max_depth_m":9.144,"surface_area_m2":190202.25185280002,"latitude":45.6047036,"nearby_city_name":"avon","lake_name":"achman","country":"USA","nearby_city_longitude":undefined},
+  {"longitude":"-95.82589709999999","id":3,"state_or_province":"minnesota","nearby_city_latitude":undefined,"max_depth_m":2.1336,"surface_area_m2":1088604.3776256,"latitude":"46.7584275","nearby_city_name":"detroit lakes","lake_name":"abbey","country":"USA","nearby_city_longitude":undefined},
+  {"longitude":"-94.40303660000001","id":7,"state_or_province":"minnesota","nearby_city_latitude":undefined,"max_depth_m":9.144,"surface_area_m2":190202.25185280002,"latitude":"45.6047036","nearby_city_name":"avon","lake_name":"achman","country":"USA","nearby_city_longitude":undefined},
   // {"longitude": 44.9812577, "latitude": -93.2716135, "id": 100000}
 ]
 

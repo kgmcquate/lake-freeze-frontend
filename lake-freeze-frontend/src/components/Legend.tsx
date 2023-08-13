7b78c1ebe0969cc2af7e5c8546b581d293016d8b
Kevin McQuate <kgmcquate@gmail.com>
2023-08-13 13:23:59 -0500

    adding custom icons and legend

--- lake-freeze-frontend/src/components/Legend.tsx
+++ lake-freeze-frontend/src/components/Legend.tsx
@@ -1,11 +1,5 @@
 import React from 'react'
 
-
-// import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid'
-import Item from '@mui/material/Grid'
-
-
 import Divider from '@mui/material/Divider';
 
 import '../styles/Map.css';
@@ -13,38 +7,14 @@ import '../styles/Legend.css';
 
 export function Legend() {
     return (
-        
-        // <Grid 
-        //     container 
-        //     direction="row"
-        //     justifyContent="center"
-        //     alignItems="center" 
-        //     columns={16}
-        //     className='legend'
-        //     columnSpacing={"12px"}
-        // >
-        //     {/* <Grid > */}
-        //         <Grid item xs={"auto"}>
-        //             <Item>
-        //                 <img src='lake.svg' width={"30px"}/>= liquid
-        //             </Item>
-        //         </Grid>
-        //         <Grid item xs={"auto"}>
-        //             <Item>
-        //         <img src='lake-frozen.svg' width={"30px"}/>= frozen</Item>
-        //         </Grid>
-                
-        //     {/* </Grid> */}
-        // </Grid>
-
         <div className='legend'>
             Legend
 
             <Divider style={{margin: "3px"}} orientation="vertical" flexItem/>
             <div>
-            cluster
+                cluster
                 <div>
-                    <img src='cluster-circle.svg' width={"30px"}/>
+                    <img src='cluster-circle.svg' width={"30px"} alt=""/>
                     
                 </div>
             </div>       
@@ -53,19 +23,17 @@ export function Legend() {
             <div>
                 liquid
                 <div>
-                    <img src='lake.svg' width={"30px"}/>
+                    <img src='lake.svg' width={"30px"} alt=""/>
                 </div>
             </div>
 
             <Divider style={{margin: "3px"}} orientation="vertical" flexItem/>
             <div>
-            frozen
+                frozen
                 <div>
-                    <img src='lake-frozen.svg' width={"30px"}/>
+                    <img src='lake-frozen.svg' width={"30px"} alt=""/>
                 </div>
             </div>
-
-   
         </div>
     )
 }
